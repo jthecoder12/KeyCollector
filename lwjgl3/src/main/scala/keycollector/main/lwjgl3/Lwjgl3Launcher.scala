c2723bb7697129143c362e8546e42b0b3f50133e
jthecoder12 <deepusj@gmail.com>
2025-02-20 20:30:29 -0500

    Optimized math and added exception handling

--- lwjgl3/src/main/scala/keycollector/main/lwjgl3/Lwjgl3Launcher.scala
+++ lwjgl3/src/main/scala/keycollector/main/lwjgl3/Lwjgl3Launcher.scala
@@ -4,6 +4,7 @@ import com.badlogic.gdx.backends.lwjgl3.{Lwjgl3Application, Lwjgl3ApplicationCon
 import keycollector.main.Main
 import org.lwjgl.glfw.GLFW.{GLFW_PLATFORM, GLFW_PLATFORM_X11, glfwInitHint}
 import org.lwjgl.system.Platform
+import org.lwjgl.util.tinyfd.TinyFileDialogs.tinyfd_messageBox
 
 // Desktop launcher
 object Lwjgl3Launcher {
@@ -17,6 +18,11 @@ object Lwjgl3Launcher {
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1)
         configuration.setWindowedMode(1280, 720)
 
-        new Lwjgl3Application(new Main, configuration)
+        try new Lwjgl3Application(new Main, configuration)
+        catch
+            case e: Exception =>
+                e.printStackTrace()
+                tinyfd_messageBox("Uncaught Exception", String.format("%s%nMessage: %s%nStack trace: %s", e.getClass.getCanonicalName, e.getMessage, e.getStackTrace.mkString("Array(", ", ", ")")).replace('"', Character.MIN_VALUE).replace('\'', Character.MIN_VALUE), "ok", "error", true)
+                System.exit(1)
     }
 }
